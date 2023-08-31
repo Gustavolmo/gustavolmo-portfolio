643b24a9258a88610c0f399b11cd477c2163aee5
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-08-31 22:38:02 +0200

    (fix) testing p5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,105 +1,26 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import globalStyles from '../global.css?inline';
 	import type p5 from 'p5';
 	import P5 from 'p5-svelte';
-	import Projects from './Projects.svelte';
+	import RandomCircle from './amimationComponents/RandomCircle.svelte';
+	import LineAcrossScreen from './amimationComponents/LineAcrossScreen.svelte';
+	import OribitalRoom from './amimationComponents/OribitalRoom.svelte';
 
-	const sketch = (p5: p5) => {
-		let x = 200 //p5.windowWidth / 2; // Initial x-coordinate
-		let y = 200 // p5.windowHeight / 2; // Initial y-coordinate
-
-		let xSpeed: number; // Speed of movement along the x-axis
-		let ySpeed: number; // Speed of movement along the y-axis
-
-		p5.setup = () => {
-			p5.createCanvas(375, p5.windowHeight / 3);
-
-			const randomTimer = () => Math.random() * 1200;
-
-			xSpeed = p5.random(-3, 3);
-			ySpeed = p5.random(-3, 3);
-
-			setInterval(() => {
-				xSpeed = p5.random(-3, 3);
-				ySpeed = p5.random(-3, 3);
-			}, randomTimer());
-		};
-
-		p5.draw = () => {
-			// Trailing effect
-			p5.background(255, 0);
-
-			// Update position with some randomness
-			x += xSpeed + p5.random(-0.5, 0.5); // Add random variation to xSpeed
-			y += ySpeed + p5.random(-0.5, 0.5); // Add random variation to ySpeed
-
-			// Bouncing off walls
-			if (x < 40 || x > p5.width - 40) {
-				xSpeed *= -1; // Reverse xSpeed
-			}
-			if (y < 40 || y > p5.height - 40) {
-				ySpeed *= -1; // Reverse ySpeed
-			}
-
-			p5.fill(255);
-			p5.ellipse(x, y, 40, 40);
-		};
-	}
-
-
-
+const prop = 'GUSTAVO'
 </script>
 
-<section class="cover-section">
-	<Header />
-	<div class="canvas-container">
-		<P5 {sketch} />
-	</div>
-</section>
+<!-- <RandomCircle /> -->
+<!-- <div id="canvas-container">
+	<OribitalRoom />
+</div> -->
 
-<section class="projects">
-	<div>
-		<div class="spacer" />
-	</div>
-	<Projects />
-	<Projects />
-	<Projects />
-</section>
+
+<LineAcrossScreen {prop}/>
 
 <style>
-	.cover-section {
-		position: fixed;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		text-align: center;
-		height: 100%;
-		width: 100%;
-		overflow: auto;
-	}
-
-	.canvas-container {
+	/* #canvas-container {
 		position: absolute;
 		top: 0;
 		left: 0;
-		width: 100%;
-		height: 100%;
-		z-index: -1;
-	}
-
-	.projects {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		overflow: auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		text-align: center;
-	}
-
-	.spacer {
-		height: 90vh;
-	}
+	} */
 </style>
---
+++ src/routes/amimationComponents/LineAcrossScreen.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import type p5 from 'p5';
+	import P5 from 'p5-svelte';
+	import { onMount } from 'svelte';
+
+	let sketch: (p5: p5) => void;
+	let canvasWidth = 210;
+
+	onMount(() => {
+		const textElement = document.querySelector('.text');
+		if (textElement) {
+			canvasWidth = textElement.clientWidth -8;
+		}
+
+		sketch = (p5: p5) => {
+			let x = 0; //p5.windowWidth / 2; // Initial x-coordinate
+			let y = 0; // p5.windowHeight / 2; // Initial y-coordinate
+
+			let xSpeed: number; // Speed of movement along the x-axis
+			let ySpeed: number; // Speed of movement along the y-axis
+
+			p5.setup = () => {
+				p5.createCanvas(canvasWidth, 40);
+
+				xSpeed = canvasWidth / 20;
+				ySpeed = 0;
+			};
+
+			p5.draw = () => {
+				// Trailing effect
+				p5.background(255, 0);
+
+				// Update position
+				x += xSpeed + canvasWidth / 1000;
+				y += ySpeed + 0;
+
+				// Bouncing off walls
+				if (x < 0 || x > p5.width - 0) {
+					xSpeed *= -1; // Reverse xSpeed
+				}
+				if (y < 50 || y > p5.height - 50) {
+					ySpeed *= -1; // Reverse ySpeed
+				}
+
+				p5.fill(0);
+				p5.ellipse(x, y, 0, 88);
+			};
+		};
+	});
+
+	export let prop: string;
+</script>
+
+<section class="animation-wrap">
+	<p class="text">{prop}</p>
+	<P5 {sketch} />
+</section>
+
+<style>
+	.animation-wrap {
+		top: 0;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.text {
+		position: absolute;
+		margin: 0;
+		top: 0;
+		color: #fff;
+		font-size: 60px;
+		line-height: 44px;
+		font-weight: bolder;
+	}
+</style>
