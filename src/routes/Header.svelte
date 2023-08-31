8de202fba8c75eb62ae1007679a6e17c1e8c30c0
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-08-31 12:43:50 +0200

    (fix) animation

---
+++ src/routes/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<header class="header">
+	<h2 class="g-tag">G</h2>
+  <!-- <h3 class="subtitle">Welcome to My world</h3> -->
+</header>
+
+<style>
+	.header {
+    position: absolute;
+    height: 40px;
+    background-color: #111;
+    padding: 8px;
+  }
+
+  /* .subtitle {
+    color: white;
+    font-size: 30px;
+  } */
+
+  .g-tag {
+    color: #eee;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type p5 from 'p5';
+	import P5 from 'p5-svelte';
+	import { onMount } from 'svelte';
+
+
+	let width = 55;
+	let height = 55;
+
+	let sketch: (arg: p5) => void;
+
+	onMount(() => {
+		const windowHeight = window.innerHeight;
+		const windowWidth = window.innerWidth;
+
+		sketch = (p5: p5) => {
+			p5.setup = () => {
+				p5.createCanvas(windowHeight, windowWidth);
+			};
+
+			p5.draw = () => {
+				p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+			};
+		};
+	});
+</script>
+
+<label>
+	Width
+	<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+	{width}
+</label>
+
+<label>
+	Height
+	<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
+	{height}
+</label>
+
+<P5 {sketch} />
